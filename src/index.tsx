08dc0cae5ba9ea31ccd2cf7ced87ba66534d6b23
Johan Henrique <60210002+JohanDev6@users.noreply.github.com>
2021-08-04 18:37:11 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,14 @@ import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components'
 
-import { MainProvider } from './providers/mainProvider'
-import { MainContext } from './providers/mainProvider'
+import { MainProvider, MainContext, IProviderData } from './providers/mainProvider'
 
 import Routes from './routes';
 import GlobalStyles from './styles/globalStyles'
 
 const Root = () => {
 
-  const { data, setData } = useContext(MainContext)
+  const { data, setData } : IProviderData | any = useContext(MainContext)
 
   return(
     <MainProvider>
